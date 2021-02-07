29e3a33dc82b9cb2f1dfaf19adc3d92bb03dbf0e
RiccardoFrenner <riccardo.frenner@gmail.com>
2021-02-07 21:40:11 +0100

    Fixed Test

--- src/walberla_bridge/tests/LBWalberla_pe_coupling.cpp
+++ src/walberla_bridge/tests/LBWalberla_pe_coupling.cpp
@@ -23,9 +23,50 @@
 #include "config.hpp"
 
 #ifdef LB_WALBERLA
+#define BOOST_TEST_NO_MAIN
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(add_remove_particles) { BOOST_CHECK(1 == 2); }
+#include <LBWalberlaD3Q19MRT.hpp>
+#include <lb_walberla_init.hpp>
 
+#include <memory>
+
+using Utils::Vector3d;
+using Utils::Vector3i;
+using walberla::LBWalberlaD3Q19MRT;
+
+constexpr double viscosity = 0.4;
+constexpr Vector3d box_dimensions = {6, 6, 9};
+constexpr double agrid = 0.5;
+constexpr Vector3i grid_dimensions{int(box_dimensions[0] / agrid),
+                                   int(box_dimensions[1] / agrid),
+                                   int(box_dimensions[2] / agrid)};
+constexpr double tau = 0.34;
+constexpr double density = 2.5;
+Vector3i mpi_shape;
+constexpr unsigned int seed = 3;
+constexpr double kT = 0.0014;
+
+BOOST_AUTO_TEST_CASE(add_removes_particles) {
+  BOOST_CHECK(1 == 1);
+  auto lb = std::make_shared<LBWalberlaD3Q19MRT>(viscosity, density, agrid, tau,
+                                                 box_dimensions, mpi_shape, 1);
+}
+
+int main(int argc, char **argv) {
+  MPI_Init(&argc, &argv);
+  int n_nodes;
+
+  MPI_Comm_size(MPI_COMM_WORLD, &n_nodes);
+  MPI_Dims_create(n_nodes, 3, mpi_shape.data());
+
+  walberla_mpi_init();
+  auto res = boost::unit_test::unit_test_main(init_unit_test, argc, argv);
+  MPI_Finalize();
+  return res;
+}
+
+#else  // ifdef LB_WALBERLA
+int main(int argc, char **argv) {}
 #endif // ifdef LB_WALBERLA
