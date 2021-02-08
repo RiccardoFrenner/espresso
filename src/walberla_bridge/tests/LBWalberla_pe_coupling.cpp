72a98f6fccc790b5421b9b44b2df2d7aa4a34b7f
RiccardoFrenner <riccardo.frenner@gmail.com>
2021-02-08 12:01:52 +0100

    Added particle add and remove tests

--- src/walberla_bridge/tests/LBWalberla_pe_coupling.cpp
+++ src/walberla_bridge/tests/LBWalberla_pe_coupling.cpp
@@ -29,12 +29,15 @@
 
 #include <LBWalberlaD3Q19MRT.hpp>
 #include <lb_walberla_init.hpp>
+#include <walberla_utils.hpp>
 
 #include <memory>
 
 using Utils::Vector3d;
 using Utils::Vector3i;
 using walberla::LBWalberlaD3Q19MRT;
+using walberla::to_vector3;
+using walberla::to_vector3d;
 
 constexpr double viscosity = 0.4;
 constexpr Vector3d box_dimensions = {6, 6, 9};
@@ -48,10 +51,69 @@ Vector3i mpi_shape;
 constexpr unsigned int seed = 3;
 constexpr double kT = 0.0014;
 
-BOOST_AUTO_TEST_CASE(add_removes_particles) {
-  BOOST_CHECK(1 == 1);
+BOOST_AUTO_TEST_CASE(add_particle) {
   auto lb = std::make_shared<LBWalberlaD3Q19MRT>(viscosity, density, agrid, tau,
                                                  box_dimensions, mpi_shape, 1);
+
+  // Create particle
+  std::uint64_t uid = 12;
+  double radius = 0.1;
+  Vector3d gpos{123, 456, 789};
+  Vector3d linVel{1.0, 0.2, 0.1};
+  lb->add_pe_particle(uid, gpos, radius, linVel);
+
+  // Add force/torque
+  Vector3d force{0.1, 0.5, 0.22};
+  Vector3d torque{0.5, 0.1, 0.324};
+  lb->set_particle_force(uid, force);
+  lb->set_particle_torque(uid, torque);
+
+  auto p = lb->get_pe_particle(uid);
+
+  // Check if p is on this rank
+  if (p != nullptr) {
+    BOOST_CHECK(static_cast<uint64_t>(p->getID()) == uid);
+    BOOST_CHECK(lb->get_particle_position(uid) == gpos);
+    BOOST_CHECK(lb->get_particle_velocity(uid) == linVel);
+    BOOST_CHECK(to_vector3d(p->getForce()) == force);
+    BOOST_CHECK(to_vector3d(p->getTorque()) == torque);
+    // BOOST_CHECK(lb->get_particle_angular_velocity() == angularVel); // TODO
+    // BOOST_CHECK(lb->get_particle_orientation() == orientation); // TODO
+  } else {
+    // Check that access to particle attributes is not possible
+    BOOST_CHECK(!lb->get_particle_position(uid));
+    BOOST_CHECK(!lb->get_particle_velocity(uid));
+  }
+
+  // Check wrong uid does not exist
+  uint64_t wrong_uid = 99;
+  BOOST_CHECK(!lb->get_pe_particle(wrong_uid));
+}
+
+BOOST_AUTO_TEST_CASE(remove_particle) {
+  auto lb = std::make_shared<LBWalberlaD3Q19MRT>(viscosity, density, agrid, tau,
+                                                 box_dimensions, mpi_shape, 1);
+
+  // Create particle
+  std::uint64_t uid = 12;
+  double radius = 0.1;
+  Vector3d gpos{123, 456, 789};
+  Vector3d linVel{1.0, 0.2, 0.1};
+  lb->add_pe_particle(uid, gpos, radius, linVel);
+
+  // Add force/torque
+  Vector3d force{0.1, 0.5, 0.22};
+  Vector3d torque{0.5, 0.1, 0.324};
+  lb->set_particle_force(uid, force);
+  lb->set_particle_torque(uid, torque);
+
+  auto p = lb->get_pe_particle(uid);
+
+  lb->remove_pe_particle(uid);
+
+  // Check that particle doesn't exist on any rank
+  BOOST_CHECK(p == nullptr);
+  BOOST_CHECK(lb->get_pe_particle(uid) == nullptr);
 }
 
 int main(int argc, char **argv) {
