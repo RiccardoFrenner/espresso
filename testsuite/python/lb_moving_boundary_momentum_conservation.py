a3cce5aab7b851dd94d2d3bd69a67e787d4fbc67
RiccardoFrenner <riccardo.frenner@gmail.com>
2021-06-07 19:17:35 +0200

    Finished python walberla pe mom conservation test

--- testsuite/python/lb_moving_boundary_momentum_conservation.py
+++ testsuite/python/lb_moving_boundary_momentum_conservation.py
@@ -26,116 +26,93 @@ from itertools import count
 # Define the LB Parameters
 TIME_STEP = 1 #0.008
 AGRID = 1 #.4
-GRID_SIZE = 12
-KVISC = 5e-3 * AGRID**2 / TIME_STEP
-DENS = 1 #AGRID**(-3)
+GRID_SIZE = 54
+KVISC = 0.1 * AGRID**2 / TIME_STEP
+DENS = 1 * AGRID**(-3)
+P_UID = 0
+P_DENSITY = 1.1 * DENS
+P_RADIUS = 7.5 * AGRID
+
 F = 5.5 / GRID_SIZE**3
-# F = 0
+EXTERNAL_FORCE_DENSITY = np.array([-.7 * F, .9 * F, .8 * F])
 GAMMA = 1
 
+P_VOLUME = 4./3. * np.pi * np.power(P_RADIUS,3)
+P_MASS = P_VOLUME * P_DENSITY
 SYSTEM_VOLUME = (AGRID*GRID_SIZE)**3
-# EXTERNAL_FORCE_DENSITY = [-.7 * F, .9 * F, .8 * F]
-EXTERNAL_FORCE_DENSITY = [0., 0., 0.]
-EXT_FLUID_FORCE = SYSTEM_VOLUME * np.array(EXTERNAL_FORCE_DENSITY)
-EXT_PARTICLE_FORCE = [0., 0., -F]
-
-PE_PARAMS = ([(EXT_PARTICLE_FORCE, "External particle force")],)
-# PE_PARAMS = ([],)
-
-LB_PARAMS = {'agrid': AGRID,
-             'dens': DENS,
-             'visc': KVISC,
-             'tau': TIME_STEP,
-             'ext_force_density': EXTERNAL_FORCE_DENSITY,
-             "pe_params": PE_PARAMS}
-
+EXTERNAL_FLUID_FORCE = EXTERNAL_FORCE_DENSITY * (SYSTEM_VOLUME - P_VOLUME)
+EXT_PARTICLE_FORCE = - EXTERNAL_FLUID_FORCE
 
 class Momentum(object):
     """
-    Tests momentum conservation for an LB coupled to an extended particle,
-    where an external force is applied to the lb fluid but not to the particle.
-    The total momentum change due to impulse is approximated.
+    Tests momentum conservation for an LB coupled to an extended particle.
     """
     lbf = None
     system = espressomd.System(box_l=[GRID_SIZE * AGRID] * 3)
     system.time_step = TIME_STEP
     system.cell_system.skin = 0.01
 
-    def test(self):
-        self.system.actors.clear()
-        self.system.part.clear()
+    def test_init_velocity(self):
+        """
+        Tests momentum conservation where the particle has an initial velocity.
+        """
+        LB_PARAMS = {
+            'agrid': AGRID,
+             'dens': DENS,
+             'visc': KVISC,
+             'tau': TIME_STEP,
+             "pe_params": ([],)
+        }
+        self.lbf = espressomd.lb.LBFluidWalberla(**LB_PARAMS)
         self.system.actors.add(self.lbf)
         self.system.thermostat.set_lb(LB_fluid=self.lbf, gamma=GAMMA, seed=1)
-        np.testing.assert_allclose(
-            self.lbf.ext_force_density,
-            LB_PARAMS["ext_force_density"])
 
         # Initial momentum before integration = 0
         np.testing.assert_allclose(
             self.system.analysis.linear_momentum(), [0., 0., 0.], atol=1E-12)
 
-        p_uid = 0
-        p_initial_v = np.array([0,0,0])
-        p_radius = 2 * AGRID
-        p_density = 1.1 * DENS
+        # Add walberla particle
+        p_initial_v = np.array([0 ,0, 1e-3*AGRID])
+        self.lbf.create_particle_material("myMat", P_DENSITY)
+        self.lbf.add_particle(P_UID, self.system.box_l/2, P_RADIUS, p_initial_v, "myMat")
+        self.lbf.finish_particle_adding()
 
-        self.lbf.create_particle_material("myMat", p_density)
-        self.lbf.add_particle(p_uid, self.system.box_l/2, p_radius, p_initial_v)
+        p_pos = self.lbf.get_particle_position(P_UID)
+        p_v = self.lbf.get_particle_velocity(P_UID)
 
-        p_pos = self.lbf.get_particle_position(p_uid)
-        p_v = self.lbf.get_particle_velocity(p_uid)
-
-        # Check unit conversion
+        # Check particle attributes
         np.testing.assert_allclose(np.copy(p_pos), np.copy(self.system.box_l/2))
         np.testing.assert_allclose(np.copy(p_v), p_initial_v)
 
         get_fluid_mom = lambda : np.array(
             self.system.analysis.linear_momentum(include_particles=False))
 
-        p_mass = 4./3. * np.pi * np.power(p_radius,3) * p_density
-        initial_momentum = get_fluid_mom() + p_initial_v * p_mass
-        np.testing.assert_allclose(initial_momentum, np.copy(p_v) * p_mass)
+        initial_momentum = get_fluid_mom() + p_v * P_MASS
+        measured_momentum = get_fluid_mom() + p_v * P_MASS
+        np.testing.assert_allclose(initial_momentum, np.copy(p_v) * P_MASS)
 
-        steps_per_it = 1
-        external_impulse = 0
+        steps_per_it = 10
         print(initial_momentum)
-        for i in count(0, 1):
-            # print(f"Iteration: {i*steps_per_it}")
+        for _ in range(20):
             self.system.integrator.run(steps_per_it)
 
-            p_v = self.lbf.get_particle_velocity(p_uid)
-            p_pos = self.lbf.get_particle_position(p_uid)
+            p_v = self.lbf.get_particle_velocity(P_UID)
+            p_pos = self.lbf.get_particle_position(P_UID)
 
-            # get_particle_force only returns hydrodynamic force
-            p_f = self.lbf.get_particle_force(p_uid) + EXT_PARTICLE_FORCE
+            fluid_mom = get_fluid_mom()
+            particle_mom = p_v * P_MASS
+            measured_momentum = fluid_mom + particle_mom
 
-            measured_momentum = get_fluid_mom() + p_v * p_mass
-
-            fluid_volume = SYSTEM_VOLUME - p_mass/p_density
-            # compensation due to external impulse (dp = dt * F)
-            external_impulse += TIME_STEP * (
-                np.array(EXTERNAL_FORCE_DENSITY) * fluid_volume # Force on fluid
-                + EXT_PARTICLE_FORCE) # Force on particle
-
-            with np.printoptions(precision=2, floatmode='fixed'):
-                fm = measured_momentum - p_v * p_mass
-                pm = p_v * p_mass
-                print("| {:5.2e} | {:5.2e} | {:5.2e} | {:5.2e} |".format(
-                    external_impulse[2], measured_momentum[2], fm[2], pm[2]))
-
-            # np.testing.assert_allclose(measured_momentum,
-            #                            external_impulse, atol=1E-2)
-
-            if np.any(np.abs(p_pos - .5*self.system.box_l) > .5*self.system.box_l - 1.5*p_radius):
-                print(f"Particle ({p_pos}) too close to boundary ({self.system.box_l}).\nStopping simulation...")
-                break
+            np.testing.assert_allclose(measured_momentum,
+                                        initial_momentum, atol=1E-5)
 
 
 @utx.skipIfMissingFeatures(['LB_WALBERLA'])
 class LBWalberlaMomentum(ut.TestCase, Momentum):
 
     def setUp(self):
-        self.lbf = espressomd.lb.LBFluidWalberla(**LB_PARAMS)
+        self.system.actors.clear()
+        self.system.part.clear()
 
 
 if __name__ == "__main__":
