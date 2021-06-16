50e1a4c9104ab0c1d4df36df8cbb29755ae2f23a
RiccardoFrenner <riccardo.frenner@gmail.com>
2021-06-16 14:59:10 +0200

    Core: Fixed walberla virtual sites update and force method

--- src/core/virtual_sites/VirtualSitesWalberlaMovingBoundary.cpp
+++ src/core/virtual_sites/VirtualSitesWalberlaMovingBoundary.cpp
@@ -17,6 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "config.hpp"
+#include "forces.hpp"
 #include <mpi.h>
 #include <stdexcept>
 
@@ -24,7 +25,9 @@
 
 #include "VirtualSitesWalberlaMovingBoundary.hpp"
 #include "cells.hpp"
+#include "grid_based_algorithms/lb_walberla_instance.hpp"
 #include "grid_based_algorithms/lb_walberla_interface.hpp"
+#include "integrate.hpp"
 
 void VirtualSitesWalberlaMovingBoundary::update() const {
   // Get the properties from the pe particle with the same id as the virtual
@@ -34,56 +37,68 @@ void VirtualSitesWalberlaMovingBoundary::update() const {
     if (!p.p.is_virtual)
       continue;
 
-    auto pos = Walberla::PE_Coupling::get_particle_position(p.identity());
-    auto vel = Walberla::PE_Coupling::get_particle_velocity(p.identity());
+    auto pos = Walberla::PE_Coupling::get_particle_position(p.identity(), true);
+    auto vel = Walberla::PE_Coupling::get_particle_velocity(p.identity(), true);
 
     // Walberla and Espresso have the same domain decomposition, therefore if
     // the pe particle is at the same position as the vs, both should be on the
-    // same rank and pos e.g. should contain the position.
-    // If they are not at the same position, there should be at least a pe ghost
-    // particle on the same rank as the vs since pe particles can not travel
-    // faster than 1 LB cell per timestep and are always larger than 1 cell.
-
-    // todo: Problem is I don't update the particle map in LBWalberlaImpl when
-    // the particle goes from one block to another. Therefore this execption
-    // is expected to occur once the particle changes blocks.
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::cout << "Rank: " << rank << std::endl;
+    // same rank and e.g. 'pos' should not be empty. If they are not at the same
+    // position, there should be at least a pe ghost particle on the same rank
+    // as the vs since pe particles can not travel faster than 1 LB cell per
+    // timestep and are always larger than 1 cell.
     if (!pos) {
-      std::cout << "hghghghghghgh" << std::endl;
-      throw std::runtime_error("Pe particle and vs are not on the same rank!");
+      int rank;
+      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+      throw std::runtime_error("There is neither a local pe particle nor a "
+                               "pe ghost with uid" +
+                               std::to_string(p.identity()) +
+                               "on this rank (rank: " + std::to_string(rank) +
+                               ")");
     }
 
-    p.r.p = *pos;
-    p.m.v = *vel;
+    // LB unit conversion
+    auto agrid = lb_walberla_params()->get_agrid();
+    auto tau = lb_walberla_params()->get_tau();
+    p.r.p = *pos * agrid;
+    p.m.v = *vel * agrid / tau;
 
 #ifdef ROTATION
-    auto angular_vel =
-        Walberla::PE_Coupling::get_particle_angular_velocity(p.identity());
+    auto angular_vel = Walberla::PE_Coupling::get_particle_angular_velocity(
+        p.identity(), true);
     p.m.omega = *angular_vel;
 #endif
+
+    // verlet list update check
+    if ((p.r.p - p.l.p_old).norm2() > skin * skin) {
+      cell_structure.set_resort_particles(Cells::RESORT_LOCAL);
+    }
   }
 }
+
+// Read forces and torques from the member variables of the Espresso particle
+// and add them to the walberla pe particles.
 void VirtualSitesWalberlaMovingBoundary::back_transfer_forces_and_torques()
     const {
-
-  // Communication needs to happen here since walberla pe particles only exist
-  // on one rank and have no ghosts. Therefore the pe particle corresponding to
-  // a ghost particle on this rank cannot be accessed here to transfer forces.
   cell_structure.ghosts_reduce_forces();
 
-  // Read forces and torques from the member variables of the Espresso particle
-  // and add them to the walberla pe particles.
+  init_forces_ghosts(cell_structure.ghost_particles());
+
   for (auto &p : cell_structure.local_particles()) {
     if (!p.p.is_virtual)
       continue;
 
-    auto f = p.f.f;
-    Walberla::PE_Coupling::add_particle_force(p.identity(), f);
+    // LB unit conversion
+    auto agrid = lb_walberla_params()->get_agrid();
+    auto tau = lb_walberla_params()->get_tau();
+    auto lb_force = p.f.f * tau * tau / agrid;
+
+    Walberla::PE_Coupling::add_particle_force(p.identity(), lb_force);
+
 #ifdef ROTATION
-    auto tau = p.f.torque;
-    Walberla::PE_Coupling::add_particle_torque(p.identity(), tau);
+    // LB unit conversion
+    auto lb_torque = p.f.torque * tau * tau / (agrid * agrid);
+
+    Walberla::PE_Coupling::add_particle_torque(p.identity(), lb_torque);
 #endif
   }
 }
